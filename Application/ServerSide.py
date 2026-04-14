2e124c281f31ff139b5788c4434e003f844158ba
Jacob <115171151+JacobT2006@users.noreply.github.com>
2026-04-14 00:56:23 -0500

    Fix print statements and improve connection message

--- Application/ServerSide.py
+++ Application/ServerSide.py
@@ -9,12 +9,21 @@ from _thread import *
 from random import *
 
 def TicketThread(connectSocket):
-    print("* | Starting connection for service \n")
+    print(" * | Starting connection for service \n")
+	
     clientRequest=connectSocket.recv(1024).decode()
 	print(clientRequest)
+	# PlaceHolder for client response 
+
+
+
+
 	while(true) #input message for loop from clientRequest
+		#Placeholder for message stuffs
+		break
 
-
+	connectSocket.close()
+    print(" * | Connection closed \n")
 
 def serverMain():
 	serverPort = 12345 #create a welcome TCP socket
@@ -22,7 +31,9 @@ def serverMain():
 	#serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 	serverSocket.bind(("", serverPort))
 	serverSocket.listen(1)
-	print("* | The server is ready!")
+	print("* | The server is ready on port {serverPort}!")
+
+	# Loop to forever accept client requests
 	while True:
 		#Create connection socket when sensing new connection request
 		connectSocket,addr=serverSocket.accept()
